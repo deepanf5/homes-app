2d5af9c15de51bf750ddf50da0015ee467740bf3
deepan <deepanappu1999@gmail.com>
2025-01-20 23:37:07 +0530

    displaying the details of house

--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housinglocation works!
-    </p>
+    <section class="listng">
+      <img [src]="housingLocation.photo" [alt]="housingLocation.name" class="list-image">
+      <h2 class="listing-heading">{{housingLocation.name}}</h2>
+      <p class="listing-location">{{housingLocation.state}}</p>
+    </section>
   `,
   styleUrl: './housinglocation.component.css'
 })
 export class HousinglocationComponent {
+
+  @Input() housingLocation!:Housinglocation
 
 }
