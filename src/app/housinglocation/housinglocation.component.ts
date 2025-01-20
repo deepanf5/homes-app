44febdc3622c97bc7717ebd94764c2f0a1df14f0
deepan <deepanappu1999@gmail.com>
2025-01-20 22:56:45 +0530

    adding housing component and logo

---
+++ src/app/housinglocation/housinglocation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housinglocation',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housinglocation works!
+    </p>
+  `,
+  styleUrl: './housinglocation.component.css'
+})
+export class HousinglocationComponent {
+
+}
